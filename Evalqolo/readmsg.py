0c69090070d278e5db20ba903f89dc7d084d1491
David <david.gonon@epfl.ch>
2022-11-19 15:29:56 +0100

    read msgs classes

--- Evalqolo/readmsg.py
+++ Evalqolo/readmsg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 import sys
-import os
-import tf2_py
+#import os
+#import tf2_py
 import rospy
 import rosbag
 import numpy as np
@@ -85,27 +85,48 @@ class OdomSample:
 			self.states[i, :] = [msg.pose.pose.position.x, msg.pose.pose.position.y, 
 				msg.twist.twist.linear.x, msg.twist.twist.linear.y]
 
-class RemoteCommands
+class RemoteCommands:
+	def __init__(self, bag, t0):
+		self.bag = bag
+		n_msg = bag.get_message_count('/qolo/remote_commands')
 
-class RDStoGUI
+		self.t = np.empty((n_msg))
+		self.vw = np.empty((n_msg, 2))
+		for i, (topic, msg, t) in enumerate(bag.read_messages(topics=['/qolo/remote_commands'])):
+			self.t[i] = (t - t0).to_sec()
+			self.vw[i, :] = msg.data[1:3]
 
+class RDStoGUI:
+	def __init__(self, bag, t0):
+		self.bag = bag
+		n_msg = bag.get_message_count('/rds_to_gui')
 
-
-#/qolo/remote_commands     
-#/rds_to_gui
+		self.t = np.empty((n_msg))
+		self.vw_orig = np.empty((n_msg, 2))
+		self.vw_corr = np.empty((n_msg, 2))
+		for i, (topic, msg, t) in enumerate(bag.read_messages(topics=['/rds_to_gui'])):
+			self.t[i] = (t - t0).to_sec()
+			self.vw_orig[i, :] = [msg.nominal_command.linear, msg.nominal_command.angular] 
+			self.vw_corr[i, :] = [msg.corrected_command.linear, msg.corrected_command.angular]
 
 def test_msgs_reading(bag_file_path):
 	bag = rosbag.Bag(bag_file_path)
 	for topic, msg, t in bag.read_messages():
 		t0 = t
 		break
-	oc_reply = OCreply(bag, t0)
 	tracked_persons = TrackedPersons(bag, t0)
-	for j in range(tracked_persons.n_tracks):
-		Pxy = tracked_persons.states[tracked_persons.isdef[:, j], j, :2]
-		plt.plot(Pxy[:, 0], Pxy[:, 1])
-	plt.gca().set_aspect(1)
-	plt.show()
+	oc_reply = OCreply(bag, t0)
+	oc_call = OCcall(bag, t0)
+	odom_sample = OdomSample(bag, t0)
+	remote_commands = RemoteCommands(bag, t0)
+	rds_to_gui = RDStoGUI(bag, t0)
+
+	if False:
+		for j in range(tracked_persons.n_tracks):
+			Pxy = tracked_persons.states[tracked_persons.isdef[:, j], j, :2]
+			plt.plot(Pxy[:, 0], Pxy[:, 1])
+		plt.gca().set_aspect(1)
+		plt.show()
 
 
 if __name__ == '__main__':
