95f049c0544b0830245d9b3b5c4f874102775247
David Gonon <david.gonon@epfl.ch>
2022-12-23 15:16:34 +0100

    notes plots examples

--- accsmabsplot.m
+++ accsmabsplot.m
@@ -1,4 +1,4 @@
-Ax = (-0.5:0.01:0.5)';
+Ax = (-1:0.01:1)';
 Ay = Ax*0;
 A2 = Ax.^2 + Ay.^2;
 Z = sqrt(A2);
@@ -7,4 +7,4 @@ s = 10;
 
 Expz = exp(-2*s*Z);
 F = Z + 1/s*log(1 + Expz) - 1/s*log(2);
-plot(Ax, Z, 'k', Ax, Ax.^2, 'k--', Ax, F, 'r')
+plot(Ax, Z, 'k-.', Ax, Ax.^2, 'm', Ax, F, 'k')
